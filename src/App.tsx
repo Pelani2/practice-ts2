0cc4a4b13faff79d44f000db5b9e12443fe9cac2
Igor Zupunski <ipatheword@live.com>
2023-09-11 13:22:02 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+
+const router = createBrowserRouter(AppRoutes);
+
+const App: React.FC = () => {
+  return (
+    <RouterProvider router={router} />
+  );
+};
+
+export default App;
