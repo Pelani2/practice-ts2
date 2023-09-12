195d02d02471bc7a2518ff42b8286d998130b3a3
Igor Zupunski <ipatheword@live.com>
2023-09-12 13:20:44 +0200

    made contact

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Contact: React.FC = () => {
+    return (
+        <div className="contact-page">
+            <h1>
+                Contact Us 
+            </h1>
+            <p>
+            Phone: +1 (123) 456-7890
+            <br />
+            Email: info@example.com
+            <br />
+            Address: 123 Main Street, Some City, Some Country  
+            </p>
+        </div>
+    );
+};
+
+export default Contact;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,15 @@
 import Home from "../components/Home";
+import Contact from "../components/Contact";
 
 const AppRoutes = [
     {
         path: "/",
         element: <Home />
     },
+    {
+        path: "/contact",
+        element: <Contact />
+    }
 ];
 
 export default AppRoutes;
