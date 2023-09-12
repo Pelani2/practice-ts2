1db94a303287343dfa02d9c749a31ca6d3d8092a
Igor Zupunski <ipatheword@live.com>
2023-09-12 13:06:03 +0200

    made landing

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,6 +4,7 @@ import "./button-styles.scss";
 const buttonClassMap: Record<string, string> = {
     "login-button": "login-button",
     "signup-button": "signup-button",
+    "cta-button": "cta-button",
 };
 
 interface ButtonProps {
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,18 @@
 import NavigationBar from "../Nav";
+import HeroBanner from "../HeroBanner";
+
 
 const Home = () => {
     return (
         <div className="home">
             <NavigationBar />
+            <HeroBanner 
+                title="Welcome to our website"
+                subtitle="Discover amazing products and services"
+                backgroundImageUrl="https://th.bing.com/th/id/OIG.kgfJtXq_qSWS2D_3PUvZ?pid=ImgGn"
+                ctaText="Get Started"
+                ctaLink="/get-started"
+            />
         </div>
     );
 };
