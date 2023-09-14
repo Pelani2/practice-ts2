8d8a1bce05926953bc02118abed4ea31d13c84db
Igor Zupunski <ipatheword@live.com>
2023-09-14 13:54:36 +0200

    made label

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const variantClassMap: Record<string, string> = {
+    "form-label": "form-label",
+};
+
+interface LabelProps {
+    variant: keyof typeof variantClassMap;
+    htmlFor: string;
+    children: React.ReactNode;
+};
+
+const Label: React.FC<LabelProps> = ({ variant, htmlFor, children }) => {
+    const className = `label ${variantClassMap[variant]}`;
+
+    return (
+        <label
+            className={className}
+            htmlFor={htmlFor}
+        >
+            {children}
+        </label>
+    );
+};
+
+export default Label;
