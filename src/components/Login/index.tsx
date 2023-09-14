862c76eed1b306cf5caced368ea75577dac76c6c
Igor Zupunski <ipatheword@live.com>
2023-09-14 13:49:36 +0200

    prog

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,49 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers";
+import { yupResolver } from '@hookform/resolvers/yup';
+import Typography from "../Typography";
+import { error } from "console";
 
 const schema = yup.object().shape({
     email: yup.string().required("Email is required").email("Invalid email"),
     password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters long"),
 });
+
+const Login: React.FC = () => {
+    const { handleSubmit, control, errors } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
+
+    return (
+        <div className="login">
+            <Typography variant="">
+                Login
+            </Typography>
+            <form 
+                onSubmit={handleSubmit(onSubmit)}
+                className="login-form"
+            >
+                <div className="form-group">
+                    <label>
+                        Email:
+                    </label>
+                    <Controller 
+                        name="email"
+                        control={control}
+                        render={({ field }) => <input {...field} />}
+                    />
+                    <Typography variant="">
+                        {errors.email?.message}
+                    </Typography>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
