bc7a5c708e06bc1fa7d55a3f4224f21fd9aa5c70
Igor Zupunski <ipatheword@live.com>
2023-09-14 13:41:28 +0200

    schema

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -2,3 +2,8 @@ import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers";
+
+const schema = yup.object().shape({
+    email: yup.string().required("Email is required").email("Invalid email"),
+    password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters long"),
+});
