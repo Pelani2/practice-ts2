9304db6deca0490eda3110aebcc85bd9162f27bc
Igor Zupunski <ipatheword@live.com>
2023-09-17 14:38:23 +0200

    variant update

--- src/components/ConfirmPasswordInput/index.tsx
+++ src/components/ConfirmPasswordInput/index.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import "./CP-styles.scss";
 
+const variantClassMap: Record<string, string> = {
+    "LS-CP-input": "LS-CP-input",
+};
+
 interface ConfirmPasswordInputProps {
   name: string;
   placeholder: string;
-  field: any; 
+  field: any;
+  variant: keyof typeof variantClassMap;
 }
 
 const ConfirmPasswordInput: React.FC<ConfirmPasswordInputProps> = ({
   name,
   placeholder,
   field,
+  variant,
 }) => {
+    const className = `CP-input ${variantClassMap[variant]}`;
+
   return (
     <input
       type="password"
       name={name}
       placeholder={placeholder}
       {...field}
+      className={className}
     />
   );
 };
