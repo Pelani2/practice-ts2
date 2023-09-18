5995bad5f79ea16880da3d009643ffaa00240388
Igor Zupunski <ipatheword@live.com>
2023-09-18 13:00:11 +0200

    step

--- src/components/StartYourTrial/index.tsx
+++ src/components/StartYourTrial/index.tsx
@@ -2,4 +2,21 @@ import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import Input from '../Input';
 import "./start-trial.scss";
+
+const schema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup.string().required("Email is requried").email("Invalid email"),
+    password: yup.string().required("Password is required"),
+});
+
+const StartYourTrial: React.FC = () => {
+    const {
+        handleSubmit, 
+        control, 
+        errors
+    } = useForm({
+        resolver: yupResolver(schema),
+    });
+};
